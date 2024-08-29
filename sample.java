8f4a5c111f3196ad940ce7a303f548c62c807dfd
Anand <yannamanenianandkumar@gmail.com>
2024-08-28 21:46:58 -0400

    Completed Stack-2

--- sample.java
+++ sample.java
@@ -5,3 +5,58 @@
 
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Stack<Integer> s = new Stack<>();
+        int curr = 0;
+        int prev = 0;
+        int[] res = new int[n];
+        for (String log : logs) {
+            String[] str = log.split(":");
+            curr = Integer.parseInt(str[2]);
+            if (str[1].equals("start")) {
+                if (!s.isEmpty()) {
+                    res[s.peek()] += curr - prev;
+                }
+                s.push(Integer.parseInt(str[0]));
+                prev = curr;
+            } else {
+                res[s.pop()] += curr + 1 - prev;
+                prev = curr + 1;
+            }
+        }
+
+        return res;
+        
+    }
+}
+
+
+//valid paranthesis
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                st.push(')');
+            }
+            else if(c == '{') {
+                st.push('}');
+            }
+            else if(c == '[') {
+                st.push(']');
+            }
+            else {
+                if(st.isEmpty() || (st.peek() != c)) {
+                    return false;
+                } else {
+                    st.pop();
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
